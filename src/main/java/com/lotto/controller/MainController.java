80641bfd1c0e252c2802e8c8cfa05e2e3bd6076a
limdh <limdh93@naver.com>
2023-07-31 20:07:19 +0900

    thymeleaf index 연결

--- src/main/java/com/lotto/controller/MainController.java
+++ src/main/java/com/lotto/controller/MainController.java
@@ -1,19 +1,24 @@
 package com.lotto.controller;
 
+import com.lotto.vo.LottoNumber;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Slf4j
 public class MainController {
 
-    @GetMapping({"/getMap"})
-    public void mainCon (){
+    @RequestMapping("/index")
+    public String mainCon (Model model){
 
+        return "index";
     }
 
 }
